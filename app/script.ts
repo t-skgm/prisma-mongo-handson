89e36088f6a63a003075e5bdc3f1de2baf0a6cbf
t-skgm <skgm0301@gmail.com>
2021-09-04 20:19:12 +0900

    add starter script

--- app/script.ts
+++ app/script.ts
@@ -1,16 +1,44 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  //
+  await prisma.$connect();
+
+  //
+
+  const user = await prisma.user.create({
+    data: {
+      name: "Rich",
+      email: "hello@prisma.com",
+    },
+  });
+
+  await prisma.post.create({
+    data: {
+      userId: user.id,
+      title: "My first post",
+      body: "Lots of really interesting stuff",
+      slug: "my-first-post",
+    },
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+
+  console.dir(allUsers, { depth: null });
 }
 
 main()
-  .catch(e => {
-    throw e
+  .catch((e) => {
+    console.error("[main]", e);
+    throw e;
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
